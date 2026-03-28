4b9ecf85c7010d78567e845c691696c9c6833712
Touhidur Zaman <touhidcodes@gmail.com>
2026-03-28 23:47:51 +0600

    rmv: garbage files

--- src/app/modules/Skill/skill.constants.ts
+++
@@ -1,5 +0,0 @@
-// for all searching
-export const skillSearchableFields: string[] = ["name", "level"];
-
-// for all filtering
-export const skillFilterableFields: string[] = ["SkillCategory", "searchTerm"];
